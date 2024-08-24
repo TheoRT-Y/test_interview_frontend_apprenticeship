f444e560facf3be8bd4b93cd9f56641d8490e07a
Théo <theo@makedifferent.fr>
2024-08-24 20:47:37 +0200

    Task 2

--- javascript/exercice.js
+++ javascript/exercice.js
@@ -9,3 +9,16 @@ function getEvenNumbers(numbers) {
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evenNumbers = getEvenNumbers(numbers);
 console.log(evenNumbers);
+
+// Task 2: DOM Interaction
+
+const button = document.getElementById("myText");
+
+// Add an event listener to the button for the 'click' event
+button.addEventListener("click", function () {
+  // Select the element with the ID 'myText'
+  const textElement = document.getElementById("myText");
+
+  // Change the text of the clicked element
+  textElement.textContent = "Hello, Check!";
+});
